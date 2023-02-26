65b1a6cfa4d7725e508ddd184d3ebe6127b4d60e
Magdalena <magdalenahyra@wp.pl>
2023-02-26 13:08:12 +0100

    Edited index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './styles/reset.local.css';
 
-const root = document.getElementById('root');
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = createRoot(rootElement);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  root
+  </React.StrictMode>
 );
